1f0e400b59965ed1de16498cb76976ebd6875048
adamelengolo <holndou359@gmail.com>
2022-12-14 18:40:55 +0000

    Use MutableList()

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,7 +4,7 @@ class Person(var firstName: String, var age: Int){
 
 fun main(){
 
-    val numbers = listOf(1,2,3,4,5,6)
+    /*val numbers = listOf(1,2,3,4,5,6)
 
     println("List: $numbers")
     println("Size: ${numbers.size}")
@@ -16,5 +16,23 @@ fun main(){
     println("Last: ${numbers.last()}")
 
     println("Contains 4? ${numbers.contains(4)}")
-    println("Contains 7? ${numbers.contains(7)}")
+    println("Contains 7? ${numbers.contains(7)}")*/
+
+//    val entrees = mutableListOf<String>()
+    val entrees : MutableList<String> = mutableListOf()
+
+    println("Add noodles: ${entrees.add("noodles")}")
+    println("Add spaghetti: ${entrees.add("spaghetti")}")
+
+    val moreItems = listOf("rivioli","lasagna","fettuccine")
+    println("Add list: ${entrees.addAll(moreItems)}")
+    println("Remove spaghetti: ${entrees.remove("spaghetti")}")
+    println("Remove first element: ${entrees.removeAt(0)}")
+
+    println("Entrees: $entrees")
+
+    entrees.clear()
+    println("Entrees: $entrees")
+
+    println("Empty ?: ${entrees.isEmpty()}")
 }
