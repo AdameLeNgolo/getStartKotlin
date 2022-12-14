499c43f8a524432ac51e5bdcfd7027e55ae0a5e4
adamelengolo <holndou359@gmail.com>
2022-12-14 20:48:10 +0000

    Order Pratice done

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,47 @@
-class Person(var firstName: String, var age: Int){
-    
+//class Person(var firstName: String, var age: Int){
+//
+//}
+
+open class Item (val name: String, val price: Int)
+
+class Noodles: Item("Noodles",10){
+    override fun toString(): String {
+        return name
+    }
+}
+class Vegetables(vararg val toppings: String) : Item("Vegetables",5){
+    override fun toString(): String {
+        if (toppings.isEmpty()){
+            return "$name Chef's Choice"
+        }else{
+            return name + " " + toppings.joinToString()
+        }
+    }
+}
+
+class Order(val orderNumber: Int){
+    private val itemList = mutableListOf<Item>()
+
+    fun addItem(newItem: Item): Order{
+        itemList.add(newItem)
+        return this
+    }
+
+    fun addAll(newItems: List<Item>): Order{
+        itemList.addAll(newItems)
+        return this
+    }
+
+    fun print(){
+        println("Order #${orderNumber}")
+        var total = 0
+
+        for (item in itemList){
+            println("${item}: $${item.price}")
+            total+=item.price
+        }
+        println("Total: $${total}")
+    }
 }
 
 fun main(){
@@ -18,7 +60,7 @@ fun main(){
     println("Contains 4? ${numbers.contains(4)}")
     println("Contains 7? ${numbers.contains(7)}")*/
 
-//    val entrees = mutableListOf<String>()
+/*    val entrees = mutableListOf<String>()
     val entrees : MutableList<String> = mutableListOf()
 
     println("Add noodles: ${entrees.add("noodles")}")
@@ -34,5 +76,45 @@ fun main(){
     entrees.clear()
     println("Entrees: $entrees")
 
-    println("Empty ?: ${entrees.isEmpty()}")
+    println("Empty ?: ${entrees.isEmpty()}")*/
+
+    /*val noodles = Noodles()
+    val vegetables1 = Vegetables(listOf("Cabbage","Sprouts","Onion"))
+    val vegetables2 = Vegetables()
+
+    println(noodles)
+    println(vegetables1)
+    println(vegetables2) */
+
+    val orderList = mutableListOf<Order>()
+
+    val order1 = Order(1)
+    order1.addItem(Noodles())
+    orderList.add(order1)
+
+    val order2 = Order(2)
+    order2.addItem(Noodles())
+    order2.addItem(Vegetables())
+    orderList.add(order2)
+
+    val order3 = Order(2)
+    val items = listOf(Noodles(),Vegetables("Carrots","Beans","Celery"))
+    order3.addAll(items)
+    orderList.add(order3)
+
+    val order4 = Order(4).addItem(Noodles()).addItem(Vegetables("Cabbage","Onion"))
+    orderList.add(order4)
+
+    orderList.add(
+        Order(5)
+            .addItem(Noodles())
+            .addItem(Noodles())
+            .addItem(Vegetables("Spinach"))
+    )
+
+    for (order in orderList){
+        order.print()
+        println()
+    }
+
 }
